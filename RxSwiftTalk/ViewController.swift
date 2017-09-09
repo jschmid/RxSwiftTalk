981c34d2b2e5c34f412ebef91dc40b82a550e53f
Jonas Schmid <jonas.schmid@gmail.com>
2017-09-09 16:48:30 +0200

    Initial Commit

---
+++ RxSwiftTalk/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RxSwiftTalk
+//
+//  Created by Jonas Schmid on 09.09.17.
+//  Copyright © 2017 Liip AG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
